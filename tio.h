b6509f8c73f0195f320b1c4329cfb2677201027e
Peter Pentchev <roam@ringlet.net>
2020-04-24 13:55:59 +0300

    Declare a variable as "extern" in a header file.

--- tio.h
+++ tio.h
@@ -111,7 +111,7 @@ extern struct t_keynames_s {
   char *name;
 } t_keynames[];
 
-void (*error_msg)( const char *, ... ) __printflike(1, 2);
+extern void (*error_msg)( const char *, ... ) __printflike(1, 2);
   /* Pointer to the error message function.
    */
 
